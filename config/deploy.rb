38a004b8b84deae9670b2ee2105dbd458d2754cd
ogawakeisuke <ogawa@uniba.jp>
2012-11-15 21:25:31 +0900

    sdf

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,6 +32,12 @@ set :unicorn_config, "#{current_path}/config/unicorn.rb"
 set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
 
 
+#sqlite対策
+task :db_setup, :roles => [:db] do
+  run "mkdir -p -m 775 #{shared_path}/db"
+end
+
+
 #unicornスタート設定　完コピ
 namespace :deploy do
   task :start, :roles => :app, :except => { :no_release => true } do 
