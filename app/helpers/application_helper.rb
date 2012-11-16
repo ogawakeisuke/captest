53ee1e44f85356e38c5d5ad5264a8f53ab2c38ef
ogawakeisuke <ogawa@uniba.jp>
2012-11-16 21:21:47 +0900

    t

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
 
-
   def markdown(text)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,:autolink =>true, :gh_blockcode=>true, :hard_wrap=>true, :no_intraemphasis=>true,:fenced_code=>true, :filter_html=>true)
+
     #optionの設定
-    options = [ :autolink, :gh_blockcode, :hard_wrap, :no_intraemphasis,
-      :fenced_code, :filter_html]
+    options = [ :autolink, :gh_blockcode, :hard_wrap, :no_intraemphasis,:fenced_code, :filter_html]
     #マークダウン
-    Redcarpet.new(text, *options).to_html.html_safe
+    raw markdown.render(text)
   end
 end
